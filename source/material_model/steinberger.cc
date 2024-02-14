a4a159fc7170d3d910c7b5c3e7010329c4666a58
Ryan Stoner <rstoner1@c455-014.stampede3.tacc.utexas.edu>
2024-02-14 15:45:55 -0600

    2.5 make see fill_plastic_outputs

--- source/material_model/steinberger.cc
+++ source/material_model/steinberger.cc
@@ -461,11 +461,12 @@ namespace aspect
              // averaging chosen.
               std::vector<double>::const_iterator max_composition = std::max_element(const_volume_fractions.begin(),const_volume_fractions.end());
               plastic_yielding = isostrain_viscosities.composition_yielding[std::distance(const_volume_fractions.begin(),max_composition)];                                                                       
+          // Fill plastic outputs if they exist.
+          rheology->fill_plastic_outputs(i,volume_fractions[i],plastic_yielding,in,out,isostrain_viscosities);
           }
 	  rheology->strain_rheology.fill_reaction_outputs(in, i, rheology->min_strain_rate, plastic_yielding, out);
 
-          // Fill plastic outputs if they exist.
-          rheology->fill_plastic_outputs(i,volume_fractions[i],plastic_yielding,in,out,isostrain_viscosities);
+
           MaterialUtilities::fill_averaged_equation_of_state_outputs(eos_outputs[i], mass_fractions, volume_fractions[i], i, out);
           fill_prescribed_outputs(i, volume_fractions[i], in, out);
         }
