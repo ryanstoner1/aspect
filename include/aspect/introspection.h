b32c1093297f2920650977a3b319cd5ec3d16077
Timo Heister <timo.heister@gmail.com>
2026-07-29 10:22:47 -0400

    add Finite Element Information postprocessor

--- include/aspect/introspection.h
+++ include/aspect/introspection.h
@@ -103,6 +103,41 @@ namespace aspect
 
       return CompositionalFieldDescription::Type();
     }
+
+    /**
+     * This function translates a compositional field type into the string
+     * used in the input file and diagnostic output.
+     */
+    static
+    std::string
+    type_to_string(const Type type)
+    {
+      switch (type)
+        {
+          case chemical_composition:
+            return "chemical composition";
+          case stress:
+            return "stress";
+          case strain:
+            return "strain";
+          case grain_size:
+            return "grain size";
+          case porosity:
+            return "porosity";
+          case density:
+            return "density";
+          case entropy:
+            return "entropy";
+          case generic:
+            return "generic";
+          case unspecified:
+            return "unspecified";
+          default:
+            AssertThrow(false, ExcInternalError());
+        }
+
+      return "";
+    }
   };
 
   /**
